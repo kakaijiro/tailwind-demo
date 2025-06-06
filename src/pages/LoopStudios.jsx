640f8d1db22b21542067386905cd7459439e59f8
kakaijiro <kakaijiro@gmail.com>
2025-06-06 19:09:21 +0900

    feat: add footer section with navigation and social links to Loop Studios page

--- src/pages/LoopStudios.jsx
+++ src/pages/LoopStudios.jsx
@@ -267,6 +267,96 @@ function LoopStudios() {
           </div>
         </div>
       </section>
+      {/* footer */}
+      <footer className="bg-black">
+        {/* container */}
+        <div className="container max-w-6xl py-10 mx-auto">
+          {/* footer flex container */}
+          <div className="flex flex-col items-center mb-8 space-y-6 md:flex-row md:space-y-0 md:justify-between md:items-start">
+            {/* menu & logo container */}
+            <div className="flex flex-col items-center space-y-8 md:items-start md:space-y-4">
+              <div className="h-8">
+                {/* logo */}
+                <img
+                  src="/loopStudios/logo.svg"
+                  alt="logo"
+                  className="w-44 md:ml-3"
+                />
+              </div>
+              {/* menu container */}
+              <div className="flex flex-col items-center space-y-4 font-bold text-white md:flex-row md:space-y-0 md:space-x-6 md:ml-3">
+                {/* imte 1 */}
+                <div className="h-10 group">
+                  <a href="#">About</a>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+                {/* imte 2 */}
+                <div className="h-10 group">
+                  <a href="#">Carrers</a>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+                {/* imte 3 */}
+                <div className="h-10 group">
+                  <a href="#">Events</a>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+                {/* imte 4 */}
+                <div className="h-10 group">
+                  <a href="#">Products</a>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+                {/* imte 5 */}
+                <div className="h-10 group">
+                  <a href="#">Support</a>
+                  <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+                </div>
+              </div>
+            </div>
+            {/* social & copywright container */}
+            <div className="flex flex-col items-start justify-between space-y-4 text-gray-500">
+              {/* icon container */}
+              <div className="flex items-center justify-center mx-auto space-x-4 md:justify-end md:mx-0">
+                {/* icon 1 */}
+                <div className="h-8 group">
+                  <img
+                    src="/loopStudios/icon-facebook.svg"
+                    alt=""
+                    className="h-6"
+                  />
+                </div>
+                {/* icon 2 */}
+                <div className="h-8 group">
+                  <img
+                    src="/loopStudios/icon-twitter.svg"
+                    alt=""
+                    className="h-6"
+                  />
+                </div>
+                {/* icon 3 */}
+                <div className="h-8 group">
+                  <img
+                    src="/loopStudios/icon-pinterest.svg"
+                    alt=""
+                    className="h-6"
+                  />
+                </div>
+                {/* icon 4 */}
+                <div className="h-8 group">
+                  <img
+                    src="/loopStudios/icon-instagram.svg"
+                    alt=""
+                    className="h-6"
+                  />
+                </div>
+              </div>
+              {/* copywright */}
+              <div className="font-bold">
+                &copy; 2024 Loopstudios. All Rights Reserved
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
