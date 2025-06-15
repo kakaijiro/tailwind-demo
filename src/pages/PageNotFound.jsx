616ea484ff08d158a1ba5289ce3b63ea3372ce91
kakaijiro <kakaijiro@gmail.com>
2025-06-15 17:59:35 +0900

    feat: enhance PageNotFound component with styled layout and navigation link

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 function PageNotFound() {
-  return <div>The page you are looking for could not be found 😢</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen max-w-sm mx-auto text-center">
+      <h1 className="text-3xl font-semibold mb-6">
+        The page you are looking for could not be found 😢
+      </h1>
+      <Link
+        to="/"
+        className="inline-block bg-blue-500 text-blue-100 px-6 py-3 text-lg rounded-lg hover:bg-blue-400 duration-200"
+      >
+        Go back home
+      </Link>
+    </div>
+  );
 }
 
 export default PageNotFound;
